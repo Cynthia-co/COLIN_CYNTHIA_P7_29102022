98b49d38d91599f95205110c77b12f740db236e2
Cynthia-co <cynthiacolin@hotmail.fr>
2022-10-29 20:32:02 +0200

    sass

--- frontend/kasa/src/pages/Error.js
+++ frontend/kasa/src/pages/Error.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Error = () => {
     return (
         <div>
-            
+            <Header/>
+            <p class="code-error">404</p>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d’accueil</Link>
         </div>
     );
 };
